06288d2d5f860d173d21ba0f89671ca8f0417565
Mårten Gustafson <marten.gustafson@schibsted.com>
2018-06-09 00:57:51 +0200

    Apply "logic".

--- roman_addition/romdition.erl
+++ roman_addition/romdition.erl
@@ -1,6 +1,8 @@
 -module(romdition).
 -include_lib("eunit/include/eunit.hrl").
 
-add(_, _) -> "II".
+add(A, B) -> string:join([A, B], "").
 
 a_test() -> "II" = add("I", "I").
+b_test() -> "III" = add("I", "II").
+c_test() -> "III" = add("II", "I").
