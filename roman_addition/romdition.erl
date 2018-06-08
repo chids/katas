46cd1e85dd19da878c4e2e2ecdf4ad4b5229a5a0
Mårten Gustafson <marten.gustafson@schibsted.com>
2018-06-09 01:10:28 +0200

    Collapse IIII into IV.

--- roman_addition/romdition.erl
+++ roman_addition/romdition.erl
@@ -1,8 +1,14 @@
 -module(romdition).
 -include_lib("eunit/include/eunit.hrl").
 
-add(A, B) -> string:join([A, B], "").
+add(A, B) -> collapse(string:join([A, B], "")).
+
+collapse([$I, $I, $I, $I | Tail]) -> "IV";
+collapse([Head | Tail]) -> [Head | Tail].
 
 a_test() -> "II" = add("I", "I").
 b_test() -> "III" = add("I", "II").
 c_test() -> "III" = add("II", "I").
+d_test() -> "IV" = add("II", "II").
+e_test() -> "IV" = add("I", "III").
+f_test() -> "IV" = add("III", "I").
