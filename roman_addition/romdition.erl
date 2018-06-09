e07f0bf21c5b2b6e738de1f267881f085085a363
Mårten Gustafson <marten.gustafson@schibsted.com>
2018-06-09 12:53:11 +0200

    Expand V into IIIII and make SumIs recursive.

--- roman_addition/romdition.erl
+++ roman_addition/romdition.erl
@@ -1,15 +1,17 @@
 -module(romdition).
 -include_lib("eunit/include/eunit.hrl").
 
-add(A, B) -> sumIs(lists:flatten([toIs(A, []), toIs(B, [])])).
+add(A, B) -> sumIs(lists:flatten([toIs(A, []), toIs(B, [])]), []).
 
 toIs([$I, $V | Tail], Expanded) -> toIs(Tail, ["IIII" | Expanded]);
+toIs([$V | Tail], Is) -> toIs(Tail, ["IIIII" | Is]);
 toIs([C | Tail], Expanded) -> toIs(Tail, [C | Expanded]);
 toIs([], Expanded) -> lists:flatten(Expanded).
 
-sumIs([$I, $I, $I, $I, $I | _]) -> "V";
-sumIs([$I, $I, $I, $I]) ->  "IV";
-sumIs([Head | Tail]) -> [Head | Tail].
+sumIs([$I, $I, $I, $I, $I | Tail], Sum) -> sumIs(Tail, ["V" | Sum]);
+sumIs([$I, $I, $I, $I | Tail], Sum) -> sumIs(Tail, ["IV" | Sum]);
+sumIs([Head | Tail], Sum) -> sumIs(Tail, lists:flatten(lists:append(Sum, [Head])));
+sumIs([], Sum) -> lists:flatten(Sum).
 
 a_test() -> "II" = add("I", "I").
 b_test() -> "III" = add("I", "II").
@@ -21,3 +23,5 @@ g_test() -> "V" = add("II", "III").
 h_test() -> "V" = add("III", "II").
 i_test() -> "V" = add("I", "IV").
 j_test() -> "V" = add("IV", "I").
+k_test() -> "VI" = add("IV", "II").
+l_test() -> "VII" = add("I", "VI").
