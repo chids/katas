3526916a3d3acb2e81679e1ec91cf2a9c05f9ad7
Mårten Gustafson <marten.gustafson@schibsted.com>
2018-06-09 12:00:25 +0200

    Collapse IIIII into V.

--- roman_addition/romdition.erl
+++ roman_addition/romdition.erl
@@ -1,10 +1,15 @@
 -module(romdition).
 -include_lib("eunit/include/eunit.hrl").
 
-add(A, B) -> collapse(string:join([A, B], "")).
+add(A, B) -> sumIs(lists:flatten([toIs(A, []), toIs(B, [])])).
 
-collapse([$I, $I, $I, $I | Tail]) -> "IV";
-collapse([Head | Tail]) -> [Head | Tail].
+toIs([$I, $V | Tail], Expanded) -> toIs(Tail, ["IIII" | Expanded]);
+toIs([C | Tail], Expanded) -> toIs(Tail, [C | Expanded]);
+toIs([], Expanded) -> lists:flatten(Expanded).
+
+sumIs([$I, $I, $I, $I, $I | _]) -> "V";
+sumIs([$I, $I, $I, $I]) ->  "IV";
+sumIs([Head | Tail]) -> [Head | Tail].
 
 a_test() -> "II" = add("I", "I").
 b_test() -> "III" = add("I", "II").
@@ -12,3 +17,7 @@ c_test() -> "III" = add("II", "I").
 d_test() -> "IV" = add("II", "II").
 e_test() -> "IV" = add("I", "III").
 f_test() -> "IV" = add("III", "I").
+g_test() -> "V" = add("II", "III").
+h_test() -> "V" = add("III", "II").
+i_test() -> "V" = add("I", "IV").
+j_test() -> "V" = add("IV", "I").
